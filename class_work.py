eb4dc5ae54140bf57bf99d7830359710e21fe7bc
pallavi-ghimire <pallavi.ghimire.39@gmail.com>
2017-12-12 10:57:47 +0545

    Class work

---
+++ class_work.py
@@ -0,0 +1,21 @@
+ x = [3,4,1,'a','b']
+     (x.index('a'))
+
+ y = ['p','q']
+ x.extend(y)
+ print(x)
+
+ x.pop()
+ print(x)
+
+ x.append('xyz')
+ print(x)
+
+ #sorting can not take place in the given list
+
+ x.remove(1)
+print(x)
+
+print(88 in x)
+
+print(len(x))
